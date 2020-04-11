929e6ee6f19aec925fd1082b0f006323296336ba
Varlop Nagelfar <holger.kleist@gmail.com>
2020-04-11 12:24:34 +0100

    v1.00
    Dies ist die org. Datein welche ich in den ersten Videos verwendet habe.

--- steamcmd/steamapps/common/DayZServer/Server_Start_Livonia.bat
+++ steamcmd/steamapps/common/DayZServer/Server_Start_Livonia.bat
@@ -9,6 +9,6 @@ cd "C:\Program Files (x86)\Steam\steamapps\common\DayZServer"
 
 :watchdog
 echo (%time%) %watch% gestartet.
-start "DayZ Server" /wait "DayZServer_x64.exe" -config=serverDZ.cfg -port=2302 -dologs -adminlog -netlog -freezecheck "-mod=@RPCFramework;@Permissions-Framework;@Community-Online-Tools;"
+start "DayZ Server" /wait "DayZServer_x64.exe" -config=serverDZ_Livonia.cfg -port=2302 -dologs -adminlog -netlog -freezecheck "-mod=@RPCFramework;@Permissions-Framework;@Community-Online-Tools;"
 echo (%time%) %watch% wurde geschlossen oder ist abgestürzt, Neustart wird veranlasst.
 goto watchdog
