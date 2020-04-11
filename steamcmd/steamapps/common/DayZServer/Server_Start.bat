085000766701c3375ad3dc7f4f0d4f3b87360f46
Varlop Nagelfar <holger.kleist@gmail.com>
2020-04-11 14:20:22 +0100

    Update
    Hatte die falsche Datei hochgeladen. Diese verweist nun auf SteamCMD

--- steamcmd/steamapps/common/DayZServer/Server_Start.bat
+++ steamcmd/steamapps/common/DayZServer/Server_Start.bat
@@ -5,10 +5,13 @@ set watch=DayZ Server
 
 title %watch% Watchdog
  
-cd "C:\Program Files (x86)\Steam\steamapps\common\DayZServer"
-
 :watchdog
+cd "C:\steamcmd" 
+start "Steam CMD Update" /wait "steamcmd.exe" +runscript steamcmd_script.txt
+
+cd "C:\steamcmd\steamapps\common\DayZServer"
+
 echo (%time%) %watch% gestartet.
 start "DayZ Server" /wait "DayZServer_x64.exe" -config=serverDZ.cfg -port=2302 -dologs -adminlog -netlog -freezecheck "-mod=@RPCFramework;@Permissions-Framework;@Community-Online-Tools;"
-echo (%time%) %watch% wurde geschlossen oder ist abgestürzt, Neustart wird veranlasst.
+echo (%time%) %watch% wurde geschlossen oder ist abgestuerzt, Neustart wird veranlasst.
 goto watchdog
